88213e6909ac65d96be7f7acc6decbf48c18f027
Vibha Chandrasekar <vibhachandrasekar12345@gmail.com>
2024-11-14 21:06:27 -0800

    updated scrolling

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+//add smooth scrolling
+const anchors = document.querySelectorAll('a[href^="#"]');
+for (let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener('click', function(event) {
+        event.preventDefault()
+        const sectionID = this.getAttribute('href').slice(1)
+        const section = document.getElementById(sectionID)
+
+        section.scrollIntoView ({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
